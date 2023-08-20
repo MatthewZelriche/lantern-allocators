9ea2b5a84f159d062ab60b9fc650e8f8a79dc7ec
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-08-20 08:31:25 -0600

    Tests for SegmentMetadata

--- src/memory_segmenter/mod.rs
+++ src/memory_segmenter/mod.rs
@@ -71,8 +71,78 @@ impl SegmentMetadata {
 
 #[cfg(test)]
 mod tests {
+    extern crate alloc;
+    use core::{alloc::Layout, ptr::null_mut};
+
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn segment_metadata() {
+        const MIB: usize = 1048576;
+        let mem = unsafe { alloc::alloc::alloc(Layout::from_size_align(1024, MIB).unwrap()) };
+
+        let segment1_ptr = mem as *mut SegmentMetadata;
+        unsafe {
+            core::ptr::write(
+                segment1_ptr,
+                SegmentMetadata::new(null_mut(), 64, true, false),
+            )
+        };
+        let segment1_ref = unsafe { segment1_ptr.as_mut().unwrap() };
+        assert_eq!(segment1_ref.addr(), segment1_ptr);
+        assert_eq!(segment1_ref.alloc_start_ptr(), unsafe {
+            (segment1_ptr as *mut u8).add(size_of::<SegmentMetadata>())
+        });
+        assert_eq!(segment1_ref.in_use(), true);
+        assert_eq!(segment1_ref.next(), None);
+        assert_eq!(segment1_ref.prev(), null_mut());
+        assert_eq!(segment1_ref.size(), 64);
+        assert_eq!(
+            segment1_ref.size_allocable(),
+            64 - size_of::<SegmentMetadata>()
+        );
+        let segment2_ptr = (unsafe { mem.add(64) } as *mut SegmentMetadata);
+        unsafe {
+            core::ptr::write(
+                segment2_ptr,
+                SegmentMetadata::new(segment1_ptr, 512, false, false),
+            )
+        };
+        segment1_ref.set_next_exists(true);
+        let segment2_ref = unsafe { segment1_ref.next().unwrap().as_mut() }.unwrap();
+        assert_eq!(segment2_ref.addr(), segment2_ptr);
+        assert_eq!(segment2_ref.alloc_start_ptr(), unsafe {
+            (segment2_ptr as *mut u8).add(size_of::<SegmentMetadata>())
+        });
+        assert_eq!(segment2_ref.in_use(), false);
+        assert_eq!(segment2_ref.next(), None);
+        assert_eq!(segment2_ref.prev(), segment1_ptr);
+        assert_eq!(segment2_ref.size(), 512);
+        assert_eq!(
+            segment2_ref.size_allocable(),
+            512 - size_of::<SegmentMetadata>()
+        );
+
+        let segment3_ptr = (unsafe { mem.add(512 + 64) } as *mut SegmentMetadata);
+        unsafe {
+            core::ptr::write(
+                segment3_ptr,
+                SegmentMetadata::new(segment2_ptr, 32, false, false),
+            )
+        };
+        segment2_ref.set_next_exists(true);
+        let segment3_ref = unsafe { segment2_ref.next().unwrap().as_mut() }.unwrap();
+        assert_eq!(segment3_ref.addr(), segment3_ptr);
+        assert_eq!(segment3_ref.alloc_start_ptr(), unsafe {
+            (segment3_ptr as *mut u8).add(size_of::<SegmentMetadata>())
+        });
+        assert_eq!(segment3_ref.in_use(), false);
+        assert_eq!(segment3_ref.next(), None);
+        assert_eq!(segment3_ref.prev(), segment2_ptr);
+        assert_eq!(segment3_ref.size(), 32);
+        assert_eq!(
+            segment3_ref.size_allocable(),
+            32 - size_of::<SegmentMetadata>()
+        );
+    }
 }
